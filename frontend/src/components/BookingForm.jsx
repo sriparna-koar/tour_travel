dc4487a14bc1a3704e58de06e265a2272387b71c
sriparna-koar <koarsk03@gmail.com>
2024-03-22 21:27:28 +0530

    firdt

--- frontend/src/components/BookingForm.jsx
+++ frontend/src/components/BookingForm.jsx
@@ -22,7 +22,7 @@ const BookingForm = () => {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      await axios.post('https://tour-travel-uuoe.onrender.com//create', formData);
+      await axios.post('https://tour-travel-uuoe.onrender.com/create', formData);
       alert('Booking created successfully');
       // Clear form after submission
       setFormData({
